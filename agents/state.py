1bfdf61d9f003b11d961055a79f1396d2ed6b10f
IngIppoliti <ing.ippoliti@gmail.com>
2025-07-07 16:18:59 +0200

    Create state.py

---
+++ agents/state.py
@@ -0,0 +1,17 @@
+# A agent that want to keep track of:
+  # task
+  # plan 
+  # draft of the essay
+  # critique of theessay
+  # list of documents 
+  # number of revision we made
+  # maximum number of 
+
+class AgentState(TypedDict):
+    task: str  
+    plan: str
+    draft: str
+    critique: str
+    content: List[str]
+    revision_number: int
+    max_revisions: int
