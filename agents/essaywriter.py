54dbaf012765d79b3859782748dfd65df7805f94
IngIppoliti <ing.ippoliti@gmail.com>
2025-07-10 11:43:41 +0200

    Update essaywriter.py

--- agents/essaywriter.py
+++ agents/essaywriter.py
@@ -41,15 +41,15 @@ def plan_node(state: AgentState):
     response = model.invoke(messages)
     return {"plan": response.content}
 
-
+#AGENT that take the OUTLINE AND PERFORM EXTRACT SOME QUERIES 
 def research_plan_node(state: AgentState):
-    queries = model.with_structured_output(Queries).invoke([
+    queries = model.with_structured_output(Queries).invoke([              #to force that the output will be something with a structure (in this case the object Queries)
         SystemMessage(content=RESEARCH_PLAN_PROMPT),
-        HumanMessage(content=state['task'])
+        HumanMessage(content=state['task'])                               #it's a task for which the agent will research the query
     ])
-    content = state['content'] or []
-    for q in queries.queries:
-        response = tavily.search(query=q, max_results=2)
-        for r in response['results']:
+    content = state['content'] or []                                      #we look the list of documents if there is the node content in the output otherwise we create an empty list
+    for q in queries.queries:                                             #we loop over the queries we generated
+        response = tavily.search(query=q, max_results=2)                  #we run the queries into Tavily
+        for r in response['results']:                                     #we put each response from each queries in the content
             content.append(r['content'])
     return {"content": content}
