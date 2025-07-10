08c88143132e1bc33e5de3a47c9840ed7019754e
IngIppoliti <ing.ippoliti@gmail.com>
2025-07-10 12:20:01 +0200

    Update essaywriter.py

--- agents/essaywriter.py
+++ agents/essaywriter.py
@@ -53,3 +53,52 @@ def research_plan_node(state: AgentState):
         for r in response['results']:                                     #we put each response from each queries in the content
             content.append(r['content'])
     return {"content": content}
+
+
+
+#AGENT WRITER that take the task and the content and write the essay
+def generation_node(state: AgentState):
+    content = "\n\n".join(state['content'] or [])
+    user_message = HumanMessage(
+        content=f"{state['task']}\n\nHere is my plan:\n\n{state['plan']}")
+    messages = [
+        SystemMessage(
+            content=WRITER_PROMPT.format(content=content)
+        ),
+        user_message
+        ]
+    response = model.invoke(messages)
+    return {
+        "draft": response.content, 
+        "revision_number": state.get("revision_number", 1) + 1
+    }
+
+#AGENT that writhe the CRITIQUE
+def reflection_node(state: AgentState):
+    messages = [
+        SystemMessage(content=REFLECTION_PROMPT), 
+        HumanMessage(content=state['draft'])
+    ]
+    response = model.invoke(messages)
+    return {"critique": response.content}
+
+#AGENT CRITIQUE RESEARCHER that perform researches on the critique that has been done and add them to the content
+def research_critique_node(state: AgentState):
+    queries = model.with_structured_output(Queries).invoke([
+        SystemMessage(content=RESEARCH_CRITIQUE_PROMPT),
+        HumanMessage(content=state['critique'])
+    ])
+    content = state['content'] or []
+    for q in queries.queries:
+        response = tavily.search(query=q, max_results=2)
+        for r in response['results']:
+            content.append(r['content'])
+    return {"content": content}
+
+
+def should_continue(state):
+    if state["revision_number"] > state["max_revisions"]:
+        return END
+    return "reflect"
+
+
